2781e73150c3afba5a03a27476d326d061348294
Zent Cash <46155884+Zentcash@users.noreply.github.com>
2019-02-18 17:51:48 +0100

    Update WalletConfig.h

--- src/config/WalletConfig.h
+++ src/config/WalletConfig.h
@@ -10,7 +10,7 @@
 namespace WalletConfig
 {
     /* The prefix your coins address starts with */
-    const std::string_view addressPrefix = "ZE";
+    const std::string_view addressPrefix = "Ze";
 
     /* Your coins 'Ticker', e.g. Monero = XMR, Bitcoin = BTC */
     const std::string ticker = "ZTC";
